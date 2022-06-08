1f4f0500a345ed2a5f65c67db89eba7fab379d29
abraham <apolichenco@gmail.com>
2022-06-08 15:47:23 -0400

    commit tree 15336b4a5fd34cade97a15f81b073162caa358db

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body:JSON.stringify({
+            name: userName, 
+            email: userEmail,
+        })
+    })
+    .then((res) => res.json())
+    .then((object) => {
+        document.querySelector('body').innerHTML += object.id
+    })
+    .catch(function (error) {
+    const message = "Unauthorized Access"
+    document.querySelector('body').innerHTML += message
+    });
+}
+
+
+
+
+
+submitData("Steve", "steve@steve.com");
+
+
+
+// fetch("http://localhost:3000/dogs", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application-json:",
+//     },
+//     body: JSON.stringify({    
+//         dogName: "Byron",
+//         dogBreed: "Poodle",})})
+//     .then((response) => response.json())
+//     .then((object) => console.log(object))
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
